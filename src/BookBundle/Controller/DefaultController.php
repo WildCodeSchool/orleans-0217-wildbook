6b44120c2e75a5a5ee17b138e8440914542b8e5e
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-05-19 17:14:22 +0200

    init projet

--- src/BookBundle/Controller/DefaultController.php
+++ src/BookBundle/Controller/DefaultController.php
@@ -12,6 +12,22 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BookBundle:Default:index.html.twig');
+        return $this->render('BookBundle:Front:index.html.twig');
+    }
+
+    /**
+     * @Route("/listwilders")
+     */
+    public function listWildersAction()
+    {
+        return $this->render('BookBundle:Front:list_wilders.html.twig');
+    }
+
+    /**
+     * @Route("/listrealisations")
+     */
+    public function listRealisationsAction()
+    {
+        return $this->render('BookBundle:Front:list_realisations.html.twig');
     }
 }
