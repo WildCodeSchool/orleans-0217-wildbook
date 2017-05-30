b8ae8e410268e919ea10151e58103cce5bbe9b49
Biovor <duriez.ludovic@gmail.com>
2017-05-30 10:31:29 +0200

    b

---
+++ src/BookBundle/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        $form = $this->$this->createFormBuilder()
+            ->add('inpute', SearchType::class, [
+                'require' => false,
+            ]);->getForm();
+
+        if ($form->isValide() )
+        return $this->render('BookBundle:Front:accueil.html.twig');
+    }
+
+
+}
