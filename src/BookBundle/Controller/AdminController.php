30cee84be2a17b74f489ddc4e2a4c1e40469209a
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-06-06 17:39:16 +0200

    footer

--- src/BookBundle/Controller/AdminController.php
+++ src/BookBundle/Controller/AdminController.php
@@ -8,8 +8,45 @@
 
 namespace BookBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Request;
 
-class AdminController
+class AdminController extends Controller
 {
+    /**
+     * @Route("/accueiladmin", name="accueil_admin")
+     */
+    public function indexAction()
+    {
+        return $this->render('BookBundle:Admin:homepage.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin_page")
+     */
+    public function adminPageAction()
+    {
+        return $this->render('BookBundle:Admin:admin.html.twig');
+    }
+
+    /**
+     * @Route("/adminwilder", name="adminwilder_page")
+     *
+     *
+     */
+    public function wilderPageAction()
+    {
+        return $this->render('BookBundle:Admin:wilder.html.twig');
+    }
+
+    /**
+     * @Route("/login_ok", name="login_ok")
+     *
+     */
+    public function showUserAction()
+    {
+        return $this->render('BookBundle:Admin:loginsuccess.html.twig');
+    }
 
 }
