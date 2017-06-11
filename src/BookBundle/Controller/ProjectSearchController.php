b34a53a93f2e54deb44fa06d1c5779a2f7899a06
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-06-11 16:29:56 +0200

    project repository en cours

--- src/BookBundle/Controller/ProjectSearchController.php
+++ src/BookBundle/Controller/ProjectSearchController.php
@@ -64,10 +64,12 @@ class ProjectSearchController extends Controller
             $projectsSearch='';
             if (isset($input)){
                 $projectsSearch = $em->getRepository(Project::class)->searchByTitle($input);
-//                dump($projectsSearch);
+                var_dump($projectsSearch);
             } else {
-                $projectsSearch = $em->getRepository(Project::class)->searchBy($categories = null,$schools = null);
-//                dump($projectsSearch);
+//                $projectsSearch = $em->getRepository(Project::class)->searchByP($promotions);
+//                $projectsSearch = $em->getRepository(Project::class)->searchByS($schools);
+                $projectsSearch = $em->getRepository(Project::class)->searchBy($categories = null,$schools = null, $promotions = null);
+                var_dump($projectsSearch);
             }
 
             return $this->render('BookBundle:Front:realisation_search.html.twig', array(
--- src/BookBundle/Repository/ProjectRepository.php
+++ src/BookBundle/Repository/ProjectRepository.php
@@ -18,23 +18,56 @@ class ProjectRepository extends \Doctrine\ORM\EntityRepository
         return $qb->getQuery()->getResult();
     }
 
-    public function searchBy($categories = null,$schools = null)
+    public function searchBy($categories = null,$schools = null, $promotions = null)
     {
-        $qb = $this->createQueryBuilder('p')
-            ->where('p.category IN (:category)')
-            ->setParameter('category', $categories)
-            ->andWhere('p.school IN (:school)')
-            ->setParameter('school', $schools);
-        return $qb->getQuery()->getResult();
+            $qb = $this->createQueryBuilder('p');
+                if($schools){$qb
+                ->where('p.school IN (:school)')
+                ->setParameter('school', $schools);
+                }
+            if($categories){$qb
+                ->andWhere('p.category IN (:category)')
+                ->setParameter('category', $categories);
+            }
+            if($promotions){$qb
+                ->leftJoin('p.school','s')
+                ->addSelect('s')
+                ->leftJoin('s.promotions','pr', 'pr.school_id = s.id')
+                ->addSelect('pr')
+                ->where('pr.id IN (:promotion)')
+                ->setParameter('promotion', $promotions);
+            }
+            return $qb->getQuery()->getResult();
+
     }
 
     public function searchByP($promotions)
     {
         $qb = $this->createQueryBuilder('p')
-            ->join('school','s')
-            ->Where('p.school IN (:school)')
-            ->setParameter('school', $schools);
+            ->leftJoin('p.school','s')
+            ->addSelect('s')
+            ->leftJoin('s.promotions','pr', 'pr.school_id = s.id')
+            ->addSelect('pr')
+            ->where('pr.id IN (:promotion)')
+            ->setParameter('promotion', $promotions);
+        var_dump($qb->getDQL());
 
+        return $qb->getQuery()->getResult();
+    }
+
+    public function searchByC($categories = null)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.category IN (:category)')
+            ->setParameter('category', $categories);
+            return $qb->getQuery()->getResult();
+    }
+
+    public function searchByS($schools = null)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.school IN (:school)')
+            ->setParameter('school', $schools);
         return $qb->getQuery()->getResult();
     }
 }
