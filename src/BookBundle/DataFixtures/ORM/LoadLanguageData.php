65cebece22248049ac13b038cd232f939f682a68
Biovor <duriez.ludovic@gmail.com>
2017-06-12 17:31:40 +0200

    LanguageData plus

--- src/BookBundle/DataFixtures/ORM/LoadLanguageData.php
+++ src/BookBundle/DataFixtures/ORM/LoadLanguageData.php
@@ -49,6 +49,46 @@ class LoadLanguageData  extends AbstractFixture implements OrderedFixtureInterfa
         $manager -> persist ( $cplus );
         $this->addReference('C++', $cplus );
 
+        $csharp = new Language ();
+        $csharp -> setLanguage('C#');
+        $manager -> persist ( $csharp );
+        $this->addReference('C#', $csharp );
+
+        $dart = new Language ();
+        $dart -> setLanguage('Dart');
+        $manager -> persist ( $dart );
+        $this->addReference('Dart', $dart );
+
+        $go = new Language ();
+        $go -> setLanguage('Go');
+        $manager -> persist ( $go );
+        $this->addReference('Go', $go );
+
+        $python = new Language ();
+        $python -> setLanguage('Python');
+        $manager -> persist ( $python );
+        $this->addReference('Python', $python );
+
+        $rust = new Language ();
+        $rust -> setLanguage('Rust');
+        $manager -> persist ( $rust );
+        $this->addReference('Rust', $rust );
+
+        $shell = new Language ();
+        $shell -> setLanguage('Shell');
+        $manager -> persist ( $shell );
+        $this->addReference('Shell', $shell );
+
+        $sql = new Language ();
+        $sql -> setLanguage('SQL');
+        $manager -> persist ( $sql );
+        $this->addReference('SQL', $sql );
+
+        $swift = new Language ();
+        $swift -> setLanguage('Swift');
+        $manager -> persist ( $swift );
+        $this->addReference('Swift', $swift );
+
         $manager -> flush ();
     }
 
