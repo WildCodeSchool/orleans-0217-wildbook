33c63f9c55d4edf71dc82c8da2ac552a3450e14f
camillecelestin <cmll.clstn@gmail.com>
2017-06-15 13:30:58 +0200

    affichage dynamique profil wilde ok

--- src/BookBundle/Controller/HomeController.php
+++ src/BookBundle/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace BookBundle\Controller;
 
+use BookBundle\Entity\Wilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
+
 
 class HomeController extends Controller
 {
@@ -14,6 +16,24 @@ class HomeController extends Controller
     public function indexAction()
     {
         return $this->render('BookBundle:Front:accueil.html.twig');
+    }
+
+    /**
+     * Shows a wilder entity.
+     *
+     * @Route("/wilder/{id}/profile", name="wilder_profile")
+     */
+    public function wilderProfileAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $wilder = $em->getRepository('BookBundle:Wilder')
+            ->findOneById($id);
+
+        return $this->render('BookBundle:Front:wilder.html.twig',array(
+            'wilder'=>$wilder
+        ));
     }
 
 }
--- src/BookBundle/Controller/WilderController.php
+++ src/BookBundle/Controller/WilderController.php
@@ -178,4 +178,5 @@ class WilderController extends Controller
             ->getForm()
         ;
     }
+
 }
