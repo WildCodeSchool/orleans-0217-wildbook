40c4f27cdcad0612df5de5f869e68ddfa5a68d69
Biovor <duriez.ludovic@gmail.com>
2017-07-05 17:06:58 +0200

    manageractivation masquer pour ilder

--- src/BookBundle/Controller/WilderController.php
+++ src/BookBundle/Controller/WilderController.php
@@ -136,7 +136,6 @@ class WilderController extends Controller
             return $this->render('wilder/edit.html.twig', array(
                 'wilder' => $wilder,
                 'edit_form' => $editForm->createView(),
-                'delete_form' => $deleteForm->createView(),
             ));
         }
     }
