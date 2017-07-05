8c958a444e26abc0b423cf25c120d0510bcb191b
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-07-05 09:53:29 +0200

    gestion compte user

--- src/BookBundle/Controller/UserController.php
+++ src/BookBundle/Controller/UserController.php
@@ -76,6 +76,8 @@ class UserController extends Controller
                 $user->setRoles(['ROLE_USER']);
                 $user->setConfirmationToken(md5(uniqid()));
 
+//                $this->redirectToRoute('fos_user_resetting_send_email');
+
 
                 $message = \Swift_Message::newInstance()
                     ->setSubject('registration')
