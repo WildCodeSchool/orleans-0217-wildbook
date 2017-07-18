4e39374291415fa4a2fbdf6e2a9eb061abfe11c9
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-07-18 23:11:01 +0200

    merge dev

--- src/BookBundle/Form/ProjectType.php
+++ src/BookBundle/Form/ProjectType.php
@@ -9,6 +9,7 @@ use BookBundle\Entity\Tag;
 use BookBundle\Entity\Technology;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
